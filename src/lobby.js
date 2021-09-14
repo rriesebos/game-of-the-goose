8eb2305c6a9309be783d4f69175fa102bb2bfd6c
rriesebos <rriesebos@hotmail.com>
2021-09-14 21:47:03 +0200

    Added match invite link with copy functionality

--- src/lobby.js
+++ src/lobby.js
@@ -24,12 +24,28 @@ export class GooseGameLobby {
         this.rootElement.innerHTML = `
             <div id="player-list"></div>
             <button id="start-match-button" class="button" disabled>Start match</button>
+
+            <div>
+                <h2>Copy the invite link:</h2>
+                <div class="copy-box">
+                    <input type="text" id="match-invite-link" class="copy-input" name="match-invite-link" readonly>
+                    <svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid meet" class="copy-icon" viewBox="0 0 24 24">
+                        <g>
+                            <path fill="none" d="M0 0h24v24H0V0z"></path>
+                            <path d="M16 1H4c-1.1 0-2 .9-2 2v14h2V3h12V1zm3 4H8c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h11c1.1 0 2-.9 2-2V7c0-1.1-.9-2-2-2zm0 16H8V7h11v14z"></path>
+                        </g>
+                    </svg>
+                </div>
+            </div>
         `;
 
         this.startMatchButton = this.rootElement.querySelector('#start-match-button');
         this.playerList = this.rootElement.querySelector('#player-list');
+        this.matchInviteLinkCopyBox = this.rootElement.querySelector('.copy-box');
+        this.matchInviteLinkInput = document.querySelector('#match-invite-link');
 
         this.startMatchButton.onclick = () => this.startMatch();
+        this.matchInviteLinkCopyBox.onclick = () => this.copyMatchInvite();
     }
 
     getMatch(matchID) {
@@ -76,6 +92,10 @@ export class GooseGameLobby {
             return;
         }
 
+        // Update match invite link
+        // TODO: update
+        this.matchInviteLinkInput.value = `localhost:1234/game.html?matchID=${this.matchID}`;
+
         // Enable start button if the player is the first player (game creator if no one leaves)
         this.startMatchButton.disabled = playerID !== '0';
 
@@ -116,6 +136,13 @@ export class GooseGameLobby {
         }
 
         this.client.moves.startGame(this.playerNames);
+    }
+
+    copyMatchInvite() {
+        this.matchInviteLinkInput.select();
+        this.matchInviteLinkInput.setSelectionRange(0, 99999);
+
+        navigator.clipboard.writeText(this.matchInviteLinkInput.value);
     }
 
     showError(errorMessage) {
