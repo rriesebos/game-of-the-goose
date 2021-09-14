f68a62a592839c1deef4e4f0562206b27610d374
rriesebos <rriesebos@hotmail.com>
2021-09-14 20:28:11 +0200

    Implemented join button disabled state updating, added redirect with matchID

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
-// TODO: add functionality to join and new match buttons, disable join if matchID parameter is not set and custom match id is empty
 // TODO: store player name in local storage
-// TODO: join button: redirect directly to the lobby using the matchID from the URL parameter/custom match id
-// TODO: create: redirect to /create.html
 
+const playerNameInput = document.querySelector('#player-name');
+const matchIdInput = document.querySelector('#match-id');
+
+const joinMatchButton = document.querySelector('#join-match-button');
 const newMatchButton = document.querySelector('#new-match-button');
+
+const params = (new URL(document.location)).searchParams;
+joinMatchButton.disabled = !params.get('matchID') && matchIdInput.value === '';
+
+matchIdInput.addEventListener('input', (e) => {
+    matchID = e.target.value;
+    joinMatchButton.disabled = !params.get('matchID') && matchID === '';
+});
+
+joinMatchButton.onclick = () => {
+    window.location.href = `/game.html?matchID=${matchID}`;
+};
+
 newMatchButton.onclick = () => {
     window.location.href = '/create.html';
 };
